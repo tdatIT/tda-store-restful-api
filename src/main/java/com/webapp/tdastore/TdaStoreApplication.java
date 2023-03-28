22334520af16f5757468039ed8b976fc236784e6
tdatIT <tdat.it2k2@gmail.com>
2023-03-28 20:18:59 +0700

    complete register and verify account

--- src/main/java/com/webapp/tdastore/TdaStoreApplication.java
+++ src/main/java/com/webapp/tdastore/TdaStoreApplication.java
@@ -1,13 +1,14 @@
 package com.webapp.tdastore;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*@EnableEncryptableProperties*/
 public class TdaStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TdaStoreApplication.class, args);
     }
-
 }
--- src/main/java/com/webapp/tdastore/config/WebSecurity.java
+++ src/main/java/com/webapp/tdastore/config/WebSecurity.java
@@ -53,7 +53,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .disable();
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/api/v1/auth/**","/verify-account/**").permitAll()
                 .antMatchers("/api/v1/products/**").permitAll()
                 .anyRequest().authenticated();
 
