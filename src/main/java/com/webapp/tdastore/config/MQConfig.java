4cc54ba6cd1f2112ff0d23ad074ab6fedc358cfc
tdatIT <tdat.it2k2@gmail.com>
2023-06-09 10:49:04 +0700

    implement rabbitMQ for handle order request

---
+++ src/main/java/com/webapp/tdastore/config/MQConfig.java
@@ -0,0 +1,43 @@
+package com.webapp.tdastore.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MQConfig {
+    public static final String QUEUE = "TDA_QUEUE";
+    public static final String EXCHANGE = "TDA_EXCHANGE";
+    public static final String ROUTING_KEY = "TDA_ROUTINGKEY";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queue).to(topicExchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    AmqpTemplate template(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
---
+++ src/main/java/com/webapp/tdastore/config/MQConsumer.java
@@ -0,0 +1,21 @@
+package com.webapp.tdastore.config;
+
+import com.webapp.tdastore.data.dto.OrderDTO;
+import com.webapp.tdastore.services.OrderService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class MQConsumer {
+    @Autowired
+    private OrderService orderService;
+
+ @RabbitListener(queues = MQConfig.QUEUE)
+    public void consumerMessageFromQueue(OrderDTO order) {
+     orderService.insertOrderForUser(order);
+     log.info("create order from mq success:" + order);
+ }
+}
